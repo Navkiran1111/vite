fdfa601808b69bacce4d14c71be997160b6fc92b
unknown <navkiran.singh1111@gmail.com>
2024-08-26 12:32:21 +0530

    2

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -2,11 +2,38 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      This is my Home<br></br>
+    <div className='rock'>
+      <br></br><h2>Web Application</h2><br></br>
+      <br></br>A web application is software that runs in your web browser. Businesses
+       have to exchange information and deliver services remotely. They use
+        web applications to connect with customers conveniently and securely.
+         The most common website features like shopping carts, product search 
+         and filtering, instant messaging, and social media newsfeeds are web
+          applications in their design. They allow you to access complex 
+          functionality without installing or configuring software.
       <br></br><br></br>
       <img src='1.jpg' />
-      
+      <h2>What are the benefits of web applications?</h2><br></br>
+      As detailed, the development process for web apps is relatively 
+      simple and cost-effective for businesses. Small teams can achieve 
+      short development cycles, making web applications an efficient 
+       affordable method of building computer programs. In addition, because
+        the same version works across all modern browsers and devices,
+         you won't have to create several different iterations for multiple
+          platforms<br></br><br></br><br></br>
+          <h2>What is the difference between a web application and a website?</h2>
+      <br></br>When the internet was newly invented, websites had significantly
+       less functionality than web apps. They were only capable of delivering
+        information to users through static content. You had to install and 
+        run software with complex functionality. Web apps were built to
+         bridge the gap between software and static sites. They had 
+         functionality and interactive user elements like the software but 
+         were delivered using a web browser URL. 
+
+However, web technology has evolved significantly since then. Most modern 
+websites are complex web applications in their design.
+
+
     </div>
     
   )
